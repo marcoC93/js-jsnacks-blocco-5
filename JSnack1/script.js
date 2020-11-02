9caddbb5d2b292de47a165a3c3b61d51779b760b
Marco <marco.chianese93@gmail.com>
2020-11-03 00:22:56 +0100

    stampa su console del numero di kg bici piu leggera

--- JSnack1/script.js
+++ JSnack1/script.js
@@ -4,49 +4,56 @@
 
 var listino_bici = [
     {
-        'nome' : 'mountain bike',
-        'peso' :  5,
+        "nome" : "mountain bike",
+        "peso" :  5,
     },
-    // {
-    //     'nome' : 'bici da corsa' ,
-    //     'peso' :  3,
-    // },
-    // {
-    //     'nome' :  'fat bike',
-    //     'peso' :  10 ,
-    // },
-    // {
-    //     'nome' : 'graziella' ,
-    //     'peso' :  7 ,
-    // },
-    // {
-    //     'nome' :  'bmx',
-    //     'peso' :  4 ,
-    // },
+    {
+        "nome" : "bici da corsa" ,
+        "peso" :  3,
+    },
+    {
+        "nome" :  "fat bike",
+        "peso" :  10 ,
+    },
+    {
+        "nome" : "graziella" ,
+        'peso' :  7 ,
+    },
+    {
+        "nome" :  "bmx",
+        "peso" :  4 ,
+    },
 ];
 
 
+var bici_leggera = 100 ;
 
 
-for (var key in listino_bici)
-    {
-        console.log(listino_bici.nome);
+
+
+
+
+// scorriamo la lista degli oggetti
+for (var i = 0; i < listino_bici.length; i++) {
+
+
+ //console.log(listino_bici[i].peso);
+    // qua abbiamo l'oggetto
+    var bici_corrente = listino_bici[i].peso ;
+    //console.log(bici_corrente);
+
+
+    if (bici_corrente < bici_leggera ){
+        bici_leggera = bici_corrente ;
     }
 
 
 
-
-
-
-for (var i = 0; i < listino_bici.length; i++) {
-
-
-    var bici_corrente = listino_bici[i] ;
-
-
-
+    //var peso = bici_corrente.peso;
 
      //console.log(listino_bici[i]);
 
 
 }
+
+console.log(bici_leggera);
