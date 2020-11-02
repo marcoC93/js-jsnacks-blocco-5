1f2bd8ca688c56d3619ec90a6cb557b9bb858983
Marco <marco.chianese93@gmail.com>
2020-11-02 17:11:52 +0100

    Inizializzazione array jsnack 1

---
+++ JSnack1/script.js
@@ -0,0 +1,52 @@
+ // Creare un array di oggetti: ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore.
+
+
+
+var listino_bici = [
+    {
+        'nome' : 'mountain bike',
+        'peso' :  5,
+    },
+    // {
+    //     'nome' : 'bici da corsa' ,
+    //     'peso' :  3,
+    // },
+    // {
+    //     'nome' :  'fat bike',
+    //     'peso' :  10 ,
+    // },
+    // {
+    //     'nome' : 'graziella' ,
+    //     'peso' :  7 ,
+    // },
+    // {
+    //     'nome' :  'bmx',
+    //     'peso' :  4 ,
+    // },
+];
+
+
+
+
+for (var key in listino_bici)
+    {
+        console.log(listino_bici.nome);
+    }
+
+
+
+
+
+
+for (var i = 0; i < listino_bici.length; i++) {
+
+
+    var bici_corrente = listino_bici[i] ;
+
+
+
+
+     //console.log(listino_bici[i]);
+
+
+}
